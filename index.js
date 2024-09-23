24834c845e746710b61288cb623e48f3922647d6
Anthony mwaura <antony.mwaura@student.moringaschool.comc0deanthony@anthony-HP-EliteBook-840-G5:~/f11$ git config --global user.email antony.mwaura@student.moringaschool.com>
2024-09-23 19:48:25 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = "Your Name"; // Replace "Your Name" with the actual user name
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+module.exports = { currentUser, welcomeMessage, excitedWelcomeMessage, shortGreeting };
